028b79f888fe897d09d4dddd926c42a476c9ddc9
qntm <qntm@users.noreply.github.com>
2020-12-17 22:52:42 +0000

    Tests for removed leap second

--- src/converter.spec.js
+++ src/converter.spec.js
@@ -250,4 +250,165 @@ describe('Converter', () => {
       })
     })
   })
+
+  describe('removed leap second', () => {
+    const twoBlocks = munge([
+      [BigInt(Date.UTC(1970, JAN, 1)), 0n],
+      [BigInt(Date.UTC(1980, JAN, 1)), -1_000_000_000_000n],
+    ])
+    const converter = Converter(twoBlocks)
+
+    describe('conversions grouped by instant', () => {
+      it('start of time', () => {
+        expect(converter.oneToMany.unixToAtomicPicos(0)).toEqual([0n])
+        expect(converter.oneToMany.unixToAtomic(0)).toEqual([0])
+        expect(converter.oneToMany.atomicToUnix(0)).toBe(0)
+        expect(converter.oneToOne.unixToAtomicPicos(0)).toBe(0n)
+        expect(converter.oneToOne.unixToAtomic(0)).toBe(0)
+        expect(converter.oneToOne.atomicToUnix(0)).toBe(0)
+      })
+
+      it('millisecond before removed leap second discontinuity begins', () => {
+        expect(converter.oneToMany.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toEqual([
+            BigInt(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)) * picosPerMilli
+          ])
+        expect(converter.oneToMany.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toEqual([
+            Date.UTC(1979, DEC, 31, 23, 59, 58, 999)
+          ])
+        expect(converter.oneToMany.atomicToUnix(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toBe(Date.UTC(1979, DEC, 31, 23, 59, 58, 999))
+
+        expect(converter.oneToOne.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toBe(BigInt(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)) * picosPerMilli)
+        expect(converter.oneToOne.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toBe(Date.UTC(1979, DEC, 31, 23, 59, 58, 999))
+        expect(converter.oneToOne.atomicToUnix(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toBe(Date.UTC(1979, DEC, 31, 23, 59, 58, 999))
+      })
+
+      it('first instant of the Unix time discontinuity: one Unix time is zero TAI times', () => {
+        expect(converter.oneToMany.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toEqual([])
+        expect(converter.oneToMany.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toEqual([])
+        // no atomicToUnix can return the value above
+
+        expect(() => converter.oneToOne.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toThrowError('No TAI equivalent: 315532799000')
+        expect(() => converter.oneToOne.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toThrowError('No TAI equivalent: 315532799000')
+        // no atomicToUnix can return the value above
+      })
+
+      it('final instant of the Unix time discontinuity: one Unix time is two TAI times', () => {
+        expect(converter.oneToMany.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 59, 999)))
+          .toEqual([])
+        expect(converter.oneToMany.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 59, 999)))
+          .toEqual([])
+        // no atomicToUnix can return the value above
+
+        expect(() => converter.oneToOne.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 59, 999)))
+          .toThrowError('No TAI equivalent: 315532799999')
+        expect(() => converter.oneToOne.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 59, 999)))
+          .toThrowError('No TAI equivalent: 315532799999')
+        // no atomicToUnix can return the value above
+      })
+
+      it('millisecond after discontinuity ends', () => {
+        expect(converter.oneToMany.unixToAtomicPicos(Date.UTC(1980, JAN, 1, 0, 0, 0, 0)))
+          .toEqual([
+            BigInt(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)) * picosPerMilli
+          ])
+        expect(converter.oneToMany.unixToAtomic(Date.UTC(1980, JAN, 1, 0, 0, 0)))
+          .toEqual([
+            Date.UTC(1979, DEC, 31, 23, 59, 59, 0)
+          ])
+        expect(converter.oneToMany.atomicToUnix(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toBe(Date.UTC(1980, JAN, 1, 0, 0, 0))
+
+        expect(converter.oneToOne.unixToAtomicPicos(Date.UTC(1980, JAN, 1, 0, 0, 0, 0)))
+          .toBe(BigInt(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)) * picosPerMilli)
+        expect(converter.oneToOne.unixToAtomic(Date.UTC(1980, JAN, 1, 0, 0, 0)))
+          .toBe(Date.UTC(1979, DEC, 31, 23, 59, 59, 0))
+        expect(converter.oneToOne.atomicToUnix(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toBe(Date.UTC(1980, JAN, 1, 0, 0, 0))
+      })
+    })
+
+    describe('conversions grouped by method', () => {
+      it('oneToMany.unixToAtomicPicos', () => {
+        expect(converter.oneToMany.unixToAtomicPicos(0)).toEqual([0n])
+        expect(converter.oneToMany.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toEqual([
+            BigInt(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)) * picosPerMilli
+          ])
+        expect(converter.oneToMany.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toEqual([])
+        expect(converter.oneToMany.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 59, 999)))
+          .toEqual([])
+        expect(converter.oneToMany.unixToAtomicPicos(Date.UTC(1980, JAN, 1, 0, 0, 0, 0)))
+          .toEqual([
+            BigInt(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)) * picosPerMilli
+          ])
+      })
+
+      it('oneToMany.unixToAtomic', () => {
+        expect(converter.oneToMany.unixToAtomic(0)).toEqual([0])
+        expect(converter.oneToMany.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toEqual([
+            Date.UTC(1979, DEC, 31, 23, 59, 58, 999)
+          ])
+        expect(converter.oneToMany.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toEqual([])
+        expect(converter.oneToMany.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 59, 999)))
+          .toEqual([])
+        expect(converter.oneToMany.unixToAtomic(Date.UTC(1980, JAN, 1, 0, 0, 0)))
+          .toEqual([
+            Date.UTC(1979, DEC, 31, 23, 59, 59, 0)
+          ])
+      })
+
+      it('oneToMany.atomicToUnix', () => {
+        expect(converter.oneToMany.atomicToUnix(0)).toBe(0)
+        expect(converter.oneToMany.atomicToUnix(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toBe(Date.UTC(1979, DEC, 31, 23, 59, 58, 999))
+        expect(converter.oneToMany.atomicToUnix(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toBe(Date.UTC(1980, JAN, 1, 0, 0, 0))
+      })
+
+      it('oneToOne.unixToAtomicPicos', () => {
+        expect(converter.oneToOne.unixToAtomicPicos(0)).toBe(0n)
+        expect(converter.oneToOne.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toBe(BigInt(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)) * picosPerMilli)
+        expect(() => converter.oneToOne.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toThrowError('No TAI equivalent: 315532799000')
+        expect(() => converter.oneToOne.unixToAtomicPicos(Date.UTC(1979, DEC, 31, 23, 59, 59, 999)))
+          .toThrowError('No TAI equivalent: 315532799999')
+        expect(converter.oneToOne.unixToAtomicPicos(Date.UTC(1980, JAN, 1, 0, 0, 0, 0)))
+          .toBe(BigInt(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)) * picosPerMilli)
+      })
+
+      it('oneToOne.unixToAtomic', () => {
+        expect(converter.oneToOne.unixToAtomic(0)).toBe(0)
+        expect(converter.oneToOne.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toBe(Date.UTC(1979, DEC, 31, 23, 59, 58, 999))
+        expect(() => converter.oneToOne.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toThrowError('No TAI equivalent: 315532799000')
+        expect(() => converter.oneToOne.unixToAtomic(Date.UTC(1979, DEC, 31, 23, 59, 59, 999)))
+          .toThrowError('No TAI equivalent: 315532799999')
+        expect(converter.oneToOne.unixToAtomic(Date.UTC(1980, JAN, 1, 0, 0, 0)))
+          .toBe(Date.UTC(1979, DEC, 31, 23, 59, 59, 0))
+      })
+
+      it('oneToOne.atomicToUnix', () => {
+        expect(converter.oneToOne.atomicToUnix(0)).toBe(0)
+        expect(converter.oneToOne.atomicToUnix(Date.UTC(1979, DEC, 31, 23, 59, 58, 999)))
+          .toBe(Date.UTC(1979, DEC, 31, 23, 59, 58, 999))
+        expect(converter.oneToOne.atomicToUnix(Date.UTC(1979, DEC, 31, 23, 59, 59, 0)))
+          .toBe(Date.UTC(1980, JAN, 1, 0, 0, 0))
+      })
+    })
+  })
 })
