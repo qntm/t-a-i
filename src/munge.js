1787d0bfa1597177242c44bb0a63df89d97ea06c
qntm <qntm@users.noreply.github.com>
2022-05-20 13:42:50 +0100

    Remove some unneeded BigInt casts

--- src/munge.js
+++ src/munge.js
@@ -36,7 +36,7 @@ const NOV = 10
 
 const picosPerSecond = 1000 * 1000 * 1000 * 1000
 const picosPerMilli = 1000n * 1000n * 1000n
-const millisPerDay = 24 * 60 * 60 * 1000
+const millisPerDay = 24n * 60n * 60n * 1000n
 
 const mjdEpoch = {
   unixPicos: BigInt(Date.UTC(1858, NOV, 17)) * picosPerMilli
@@ -62,17 +62,17 @@ const munge = (data, model) => {
 
     start.unixPicos = BigInt(start.unixMillis) * picosPerMilli
 
-    root.unixPicos = mjdEpoch.unixPicos + BigInt(root.mjds) * BigInt(millisPerDay) * picosPerMilli
+    root.unixPicos = mjdEpoch.unixPicos + BigInt(root.mjds) * millisPerDay * picosPerMilli
 
     // `4.313_170_0 * 1000_000_000_000` evaluates to `4_313_170_000_000.000_5` so we must round
     offsetAtRoot.atomicPicos = BigInt(Math.round(offsetAtRoot.atomicSeconds * picosPerSecond))
 
     // `8.640_0 * 1000_000_000_000` evaluates to `8_640_000_000_000.001` so we must round
     driftRate.atomicPicosPerUnixDay = BigInt(Math.round(driftRate.atomicSecondsPerUnixDay * picosPerSecond))
-    driftRate.atomicPicosPerUnixMilli = driftRate.atomicPicosPerUnixDay / BigInt(millisPerDay)
+    driftRate.atomicPicosPerUnixMilli = driftRate.atomicPicosPerUnixDay / millisPerDay
     // Typically 15n
 
-    if (driftRate.atomicPicosPerUnixMilli * BigInt(millisPerDay) !== driftRate.atomicPicosPerUnixDay) {
+    if (driftRate.atomicPicosPerUnixMilli * millisPerDay !== driftRate.atomicPicosPerUnixDay) {
       // Rounding occurred
       throw Error('Could not compute precise drift rate')
     }
@@ -141,7 +141,7 @@ const munge = (data, model) => {
       // When smearing, this is twelve Unix hours prior to discontinuity.
       const smearStart = {
         unixPicos: model === MODELS.SMEAR
-          ? b.start.unixPicos - BigInt(millisPerDay) * picosPerMilli / 2n
+          ? b.start.unixPicos - millisPerDay * picosPerMilli / 2n
           : b.start.unixPicos
       }
 
@@ -155,7 +155,7 @@ const munge = (data, model) => {
       // When smearing, this is twelve hours after the discontinuity.
       const smearEnd = {
         unixPicos: model === MODELS.SMEAR
-          ? b.start.unixPicos + BigInt(millisPerDay) * picosPerMilli / 2n
+          ? b.start.unixPicos + millisPerDay * picosPerMilli / 2n
           : b.start.unixPicos
       }
 
@@ -202,7 +202,7 @@ const munge = (data, model) => {
   return munged.map(datum => new Segment(
     {
       atomicRatio: new Rat(datum.start.atomicPicos, 1_000_000_000_000n),
-      unixRatio: new Rat(BigInt(datum.start.unixPicos), 1_000_000_000_000n)
+      unixRatio: new Rat(datum.start.unixPicos, 1_000_000_000_000n)
     },
     {
       atomicRatio: datum.end.atomicPicos === Infinity
