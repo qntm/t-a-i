ce7ed246612a020b39198109d441c57e0cacd07a
qntm <qntm@users.noreply.github.com>
2022-05-30 20:59:46 +0100

    Philosophical changes (#39)

--- src/exact.js
+++ src/exact.js
@@ -1,10 +1,10 @@
-const { taiData, UNIX_START, UNIX_END } = require('./tai-data.js')
+const { taiData, UNIX_START_MILLIS, UNIX_END_MILLIS } = require('./tai-data.js')
 const { MODELS } = require('./munge.js')
 const { Converter } = require('./converter.js')
 const { Rat } = require('./rat.js')
 
-module.exports.UNIX_START = UNIX_START
-module.exports.UNIX_END = UNIX_END
+module.exports.UNIX_START = Rat.fromMillis(UNIX_START_MILLIS)
+module.exports.UNIX_END = Rat.fromMillis(UNIX_END_MILLIS)
 module.exports.MODELS = MODELS
 module.exports.TaiConverter = model => new Converter(taiData, model)
 module.exports.Rat = Rat
