a96253b8040ca844adb9b71dbabdad3d757a0301
qntm <qntm@users.noreply.github.com>
2022-05-30 20:57:17 +0100

    Philosophical changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const { taiData, UNIX_START, UNIX_END } = require('./tai-data.js')
+const { taiData, UNIX_START_MILLIS, UNIX_END_MILLIS } = require('./tai-data.js')
 const { MODELS } = require('./munge.js')
 const { MillisConverter } = require('./millis-converter.js')
 
-module.exports.UNIX_START = UNIX_START.toMillis()
-module.exports.UNIX_END = UNIX_END.toMillis()
+module.exports.UNIX_START = UNIX_START_MILLIS
+module.exports.UNIX_END = UNIX_END_MILLIS
 module.exports.MODELS = MODELS
 module.exports.TaiConverter = model => new MillisConverter(taiData, model)
