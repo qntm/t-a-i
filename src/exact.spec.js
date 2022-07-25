d2446790bc8cf179ed85a858f182a46ddf45d9a8
qntm <qntm@users.noreply.github.com>
2022-07-25 19:43:17 +0100

    Push `UNIX_END_MILLIS` back by 6 months (#44)
    * Push UNIX_END_MILLIS back by 6 months
    
    * Fix tests
    
    * Can you tell I haven't bothered to set up a local development environment today

--- src/exact.spec.js
+++ src/exact.spec.js
@@ -7,6 +7,7 @@ const JAN = 0
 const FEB = 1
 const MAR = 2
 const APR = 3
+const JUN = 5
 const JUL = 6
 const AUG = 7
 const SEP = 8
@@ -21,7 +22,7 @@ describe('UNIX_START', () => {
 
 describe('UNIX_END', () => {
   it('is correct', () => {
-    expect(UNIX_END).toEqual(Second.fromMillis(Date.UTC(2022, DEC, 31, 12, 0, 0, 0)))
+    expect(UNIX_END).toEqual(Second.fromMillis(Date.UTC(2023, JUN, 30, 12, 0, 0, 0)))
   })
 })
 
