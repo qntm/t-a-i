b1e139b4cdaf1302c4aa8a08b777e8d95ebc72bc
qntm <qntm@users.noreply.github.com>
2025-12-22 22:10:12 +0000

    Update millis-converter.test.js

--- test/millis-converter.test.js
+++ test/millis-converter.test.js
@@ -1,6 +1,6 @@
 import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
 
-import { describe, it } from 'mocha'
 import { MODELS } from '../src/munge.js'
 import { MillisConverter } from '../src/millis-converter.js'
 
