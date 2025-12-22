3d6db23e3bc5b832d6e5e619d640e209522f84ea
qntm <qntm@users.noreply.github.com>
2025-12-22 22:10:33 +0000

    Update munge.test.js

--- test/munge.test.js
+++ test/munge.test.js
@@ -1,6 +1,6 @@
 import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
 
-import { describe, it } from 'mocha'
 import { taiData } from '../src/tai-data.js'
 import { munge, MODELS } from '../src/munge.js'
 import { Second } from '../src/second.js'
