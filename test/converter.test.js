c1acac8b6c8eb9acfa75ac75945e3562ee171df0
qntm <qntm@users.noreply.github.com>
2025-12-22 22:17:14 +0000

    Use Node Test Runner (#84)
    * Update package.json
    
    * Update converter.test.js
    
    * Update div.test.js
    
    * Update exact.test.js
    
    * Update index.test.js
    
    * Update millis-converter.test.js
    
    * Update munge.test.js
    
    * Update nanos-converter.test.js
    
    * Update nanos.test.js
    
    * Update rat.test.js
    
    * Update second.test.js
    
    * Update segment.test.js
    
    * Update index.test.js

--- test/converter.test.js
+++ test/converter.test.js
@@ -1,6 +1,6 @@
 import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
 
-import { describe, it } from 'mocha'
 import { Converter } from '../src/converter.js'
 import { MODELS } from '../src/munge.js'
 import { Range } from '../src/range.js'
--- test/nanos.test.js
+++ test/nanos.test.js
@@ -1,6 +1,6 @@
 import assert from 'node:assert/strict'
+import { describe, it } from 'node:test'
 
-import { describe, it } from 'mocha'
 import { TaiConverter, MODELS, UNIX_START, UNIX_END } from '../src/nanos.js'
 
 const JAN = 0
