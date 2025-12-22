d3fc568efeb39467c63b024a8d8017dde868351f
qntm <qntm@users.noreply.github.com>
2025-12-22 22:15:05 +0000

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 import assert from 'node:assert/strict'
-import { describe, it } from 'node:mocha'
+import { describe, it } from 'node:test'
 
 import { TaiConverter, MODELS, UNIX_START, UNIX_END } from '../src/index.js'
 
